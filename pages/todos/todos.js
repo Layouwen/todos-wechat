a751790afd763f8c2f18898963fabcad870dbc4f
layouwen <layouwen@gmail.com>
2020-08-05 08:12:50 +0800

    完成状态切换,及其对应的数量显示

--- pages/todos/todos.js
+++ pages/todos/todos.js
@@ -5,14 +5,15 @@ Page({
       {name: 'Learning HTML', completed: true},
       {name: 'Learning CSS', completed: false},
       {name: 'Learning JavaScript', completed: true},
-    ]
+    ],
+    amount: 1
   },
   updateSearch: function(e) {
     this.setData({
       input: e.detail.value
     })
   },
-  setTodo: function(){
+  setTodo: function(e){
     if(!this.data.input) return
     let todos = this.data.todos
     this.data.todos.push({
@@ -20,7 +21,19 @@ Page({
       completed: false
     })
     this.setData({
-      todos: todos
+      todos: todos,
+      input: '',
+      amount: this.data.amount + 1
+    }) 
+  },
+  scheduleManage: function(e){
+    let index = e.currentTarget.dataset.index
+    let item = this.data.todos[index]
+    item.completed = !item.completed
+    let amount = this.data.amount + (item.completed ? -1 : 1)
+    this.setData({
+      todos: this.data.todos,
+      amount: amount
     })
   }
 })
